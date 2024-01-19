d962a8be302c61895318a194f587cdc70db82809
Samuel Henao <codinsamueldev@gmail.com>
2024-01-19 12:48:27 -0500

    bug: style products selected

--- src/cart.js
+++ src/cart.js
@@ -17,7 +17,25 @@ cartCalculation()
 
 let generateCartItems = () => {
     if (basket.length !== 0){
-        console.log("Products si hay...");
+        return (selectedProducts.innerHTML = basket.map((y) => {
+            let {id, item} = y
+            let search = shopItemsData.find((z) => z.id == id)
+            return `
+            <figure>
+                <img width="120" src=${search.img}>
+            </figure>
+            <div class="cart-items">
+                <header class="details">
+                    <div class="title-price-cross">
+                        <h3>${search.name}</h3>
+                        <b>$ ${search.price}</b>
+                        <i class="bi bi-x-lg"></i>
+                    </div>
+                    <div class="cart-buttons"></div>
+                </header>
+            </div>
+            `
+        }).join(""))
     }
     else{
         cartTotal.innerHTML = `
