006c0de3a2f5bd6d2301d3ed7f3817ae3f77711a
Samuel Henao <codinsamueldev@gmail.com>
2024-01-19 10:50:55 -0500

    necessary update to prevent app get crashed

--- src/script.js
+++ src/script.js
@@ -1,36 +1,5 @@
 let shop = document.getElementById("cards")  // Get parent tag, so now we can add information into that element.
 
-let shopItemsData = [  // Save required information into an object, then with map function access it.
-    {
-        id : 0,
-        name : "Green shirt",
-        price : 45,
-        description : "Some sample text for this card :o",
-        img : "./images/img-1.jpg"
-    }, 
-    {
-        id : 1,
-        name : "Executive shirts",
-        price : 110,
-        description : "Some sample text for this card :o",
-        img : "./images/img-2.jpg"
-    }, 
-    {
-        id : 2,
-        name : "White shirt",
-        price : 30,
-        description : "Some sample text for this card :o",
-        img : "./images/img-3.jpg"
-    }, 
-    {
-        id : 3,
-        name : "Suit",
-        price : 145,
-        description : "Some sample text for this card :o",
-        img : "./images/img-4.jpg"
-    }
-]
-
 // If we have local data stored retrieve it, if not then initialize an empty array.
 let basket = JSON.parse(localStorage.getItem("data")) || []
 
