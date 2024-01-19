cf711105cbceeb4d5dd81bcc5bd15ab6d9c5a621
Samuel Henao <codinsamueldev@gmail.com>
2024-01-19 12:04:53 -0500

    feat: when no products selected display info

--- src/cart.js
+++ src/cart.js
@@ -1,8 +1,8 @@
 
+let cartTotal = document.getElementById("cart-total")
+let selectedProducts = document.getElementById("selected-products")
+
 let basket = JSON.parse(localStorage.getItem("data")) || []
-
-
-console.log(basket)
 
 let cartCalculation = () => {
     let cartCounter = document.getElementById("cart-amount")
@@ -15,3 +15,18 @@ let cartCalculation = () => {
 
 cartCalculation()
 
+let generateCartItems = () => {
+    if (basket.length !== 0){
+        console.log("Products si hay...");
+    }
+    else{
+        cartTotal.innerHTML = `
+        <h2>Cart is empty :c</h2>
+        <a href="./index.html">
+            <button class="home-btn" type="button">Back to home-page</button>
+        </a>
+        `
+    }
+}
+
+generateCartItems()
