2f08b36faccbc4b0a47cf824577e6332bcdc2f26
Samuel Henao <codinsamueldev@gmail.com>
2024-01-27 16:46:13 -0500

    Add more products

--- src/Data.js
+++ src/Data.js
@@ -30,5 +30,69 @@ let shopItemsData = [  // Save required information into an object, then with ma
         description : "Some sample text for this card :o",
         img : "./images/img-4.jpg",
         alt : "Luxurious suit for unique ocassions"
-    }
+    },
+    {
+      id: 4,
+      name: "Mens Tie",
+      price: 25,
+      description : "Some sample text for this card :o",
+      img : "./images/img-5.png",
+      alt : "Luxurious suit for unique ocassions"
+    },
+    {
+      id: 5,
+      name: "Casual shoes",
+      price: 200,
+      description : "Some sample text for this card :o",
+      img : "./images/img-6.png",
+      alt : "Luxurious suit for unique ocassions"
+    },
+    {
+      id: 6,
+      name: "Black suit",
+      price: 450,
+      description : "Some sample text for this card :o",
+      img : "./images/img-7.png",
+      alt : "Luxurious suit for unique ocassions"
+    },
+    {
+      id: 7,
+      name: "Polo shirt",
+      price: 45,
+      description : "Some sample text for this card :o",
+      img : "./images/img-8.png",
+      alt : "Luxurious suit for unique ocassions"
+    },
+    {
+      id: 8,
+      name: "Denim shirt",
+      price: 85,
+      description : "Some sample text for this card :o",
+      img : "./images/img-9.png",
+      alt : "Luxurious suit for unique ocassions"
+    },
+    {
+      id: 9,
+      name: "Denim pants",
+      price: 120,
+      description : "Some sample text for this card :o",
+      img : "./images/img-10.png",
+      alt : "Luxurious suit for unique ocassions"
+    },
+    {
+      id: 10,
+      name: "Basic cap",
+      price: 35,
+      description : "Some sample text for this card :o",
+      img : "./images/img-11.png",
+      alt : "Luxurious suit for unique ocassions"
+    },
+    {
+      id: 11,
+      name: "Leather boots",
+      price: 350,
+      description : "Some sample text for this card :o",
+      img : "./images/img-12.png",
+      alt : "Luxurious suit for unique ocassions"
+    },
 ]
